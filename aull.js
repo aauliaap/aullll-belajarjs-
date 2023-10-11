a80bb8faf01e8bd9d10cb4253b859bf5547b31d5
aauliaap <126741616+aauliaap@users.noreply.github.com>
2023-10-11 11:20:45 +0700

    add file

---
+++ aull.js
@@ -0,0 +1,5 @@
+let siswaArray = [
+    { id: 1, nama: "John", umur: 17, kelas: "XI" },
+    { id: 2, nama: "Jane", umur: 16, kelas: "X" },
+    // tambahkan siswa lainnya jika perlu
+  ];
---
+++ aulll.js
@@ -0,0 +1,5 @@
+function celciusToFahrenheit(celcius) {
+    return celcius * 9/5 + 32;
+  }
+  
+  
